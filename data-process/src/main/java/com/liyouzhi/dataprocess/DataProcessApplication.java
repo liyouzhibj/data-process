405f6935494be7247a4b1b01ef7905d27957f63a
liyouzhi <shawshankjing@163.com>
2018-03-03 21:22:42 +0800

    first commit

---
+++ data-process/src/main/java/com/liyouzhi/dataprocess/DataProcessApplication.java
@@ -0,0 +1,12 @@
+package com.liyouzhi.dataprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessApplication.class, args);
+	}
+}
